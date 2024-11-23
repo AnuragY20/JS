b0d58121d0b427f6a555c0e0daf4f6fde93190a1
AnuragY20 <142909636+AnuragY20@users.noreply.github.com>
2024-11-23 17:48:56 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=4455
+let accountName="Anurag"
+var accountCity="Jaipur"
+accountPass="22456"
+let accountnew;
+
+accountName="Hey"
+accountCity="London"
+
+// accountId=887  can not change constant var
+
+console.log(accountId);
+console.table([accountName,accountCity,accountPass,accountnew])
